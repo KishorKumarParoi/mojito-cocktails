e999cbbfef2996a2cfef8b5de3acc7578bb56f07
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2025-12-01 12:07:34 +0600

    added Art section with mask-image animation feature

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -22,7 +22,7 @@ const About = () => {
         })
             .from('.top-grid div, .bottom-grid div', {
                 opacity: 0, yPercent: 100, duration: 1, ease: 'power1.inOut', stagger: 0.04
-            }, '-=0.5')
+            }, '-=1')
     })
 
     return (
